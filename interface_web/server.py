c09ba5d5b5cd933b59c859d242c75ebdc4af25be
Abunux <abunux@gmail.com>
2016-02-18 12:05:49 +0100

    modifié:         bn_grille.py 	nouveau fichier: interface_web/cgi-bin/bn_cgi.py 	supprimé:        interface_web/cgi-bin/duo_ordi.py 	supprimé:        interface_web/cgi-bin/ordi.py 	supprimé:        interface_web/cgi-bin/solo.py 	modifié:         interface_web/css/style_duo.css 	modifié:         interface_web/index.html 	modifié:         interface_web/server.py

--- interface_web/server.py
+++ interface_web/server.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python3
+
 import os, sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
-
-# Source pour la partie serveur: 
-# https://www.safaribooksonline.com/library/view/programming-python-4th/9781449398712/ch01s08.html
 
 # Effaçage des sessions antérieures éventuelles
 for session in os.listdir("sessions"):
 	os.remove(os.path.join("sessions",session))
 
-webdir = '.'   # where your html files and cgi-bin script directory live
-port   = 8000    # default http://localhost/, else use http://localhost:xxxx/
+# Source pour la partie serveur: 
+# https://www.safaribooksonline.com/library/view/programming-python-4th/9781449398712/ch01s08.html
+
+webdir = '.'	# Répertoire des pages html et du dossier cgi-bon
+port = 8000		# Port
 
 print("Serveur lancé")
 print("Page accessible sur http://localhost:8000/index.html")
@@ -17,7 +19,7 @@ print("----------------------------------------------------")
 print()
 
 os.chdir(webdir)                                       # run in HTML root dir
-srvraddr = ("", port)                                  # my hostname, portnumber
+srvraddr = ("", port)                                  # hostname, port
 srvrobj  = HTTPServer(srvraddr, CGIHTTPRequestHandler)
-srvrobj.serve_forever()                                # run as perpetual daemon
+srvrobj.serve_forever()                                # Lance le serveur en démon
 
